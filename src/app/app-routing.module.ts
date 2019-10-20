fd99cf1ff7a090e873b0dcb47a500267cdead2ef
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-10-20 09:50:37 +0300

    app-router static

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { navbarRouterItems, footerRouterItems } from './constants';
+// import { navbarRouterItems, footerRouterItems } from './constants';
 import { EmptyPageComponent } from './layout/empty-page/empty-page.component';
 
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     pathMatch: 'full',
+//     component: EmptyPageComponent,
+//   },
+//   ...navbarRouterItems
+//     .map(item => ({
+//       path: item.path,
+//       component: item.component,
+//     })),
+//   ...footerRouterItems
+//     .map(item => ({
+//       path: item.path,
+//       component: item.component,
+//     }))
+// ];
 
 const routes: Routes = [
   {
@@ -11,16 +29,42 @@ const routes: Routes = [
     pathMatch: 'full',
     component: EmptyPageComponent,
   },
-  ...navbarRouterItems
-    .map(item => ({
-      path: item.path,
-      component: item.component,
-    })),
-  ...footerRouterItems
-    .map(item => ({
-      path: item.path,
-      component: item.component,
-    }))
+  {
+    path: 'bank-account',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'you-bank-account',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'metal',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'business-account',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'plans',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'legal-documents',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'update-cookie-settings',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'privacy-policy',
+    component: EmptyPageComponent,
+  },
+  {
+    path: 'imprint',
+    component: EmptyPageComponent,
+  },
 ];
 
 @NgModule({
