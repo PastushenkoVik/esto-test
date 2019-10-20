32ae3f49b104121e4d8ced211261a028c1427ca6
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-10-20 09:03:56 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { navbarRouterItems } from './constants';
+import {EmptyPageComponent} from './layout/empty-page/empty-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: EmptyPageComponent,
+  },
+  ...navbarRouterItems
+    .map(item => ({
+      path: item.path,
+      component: item.component,
+    }))
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
