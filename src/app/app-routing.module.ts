b1df33cd6eeb11861686bb7d925e59c71ff82209
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-10-20 09:41:24 +0300

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { navbarRouterItems } from './constants';
-import {EmptyPageComponent} from './layout/empty-page/empty-page.component';
+import { navbarRouterItems, footerRouterItems } from './constants';
+import { EmptyPageComponent } from './layout/empty-page/empty-page.component';
 
 
 const routes: Routes = [
@@ -12,6 +12,11 @@ const routes: Routes = [
     component: EmptyPageComponent,
   },
   ...navbarRouterItems
+    .map(item => ({
+      path: item.path,
+      component: item.component,
+    })),
+  ...footerRouterItems
     .map(item => ({
       path: item.path,
       component: item.component,
