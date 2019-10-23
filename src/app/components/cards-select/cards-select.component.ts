df6e947faa5c0ee1128179d18ef595682515459d
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-10-23 09:19:04 +0300

    button-white

--- src/app/components/cards-select/cards-select.component.ts
+++ src/app/components/cards-select/cards-select.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardsSelectComponent implements OnInit {
   @Input() cardSelect;
 
+  buttonType = {isButtonGreen: true}
+
   constructor() { }
 
   ngOnInit() {
